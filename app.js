36a02ffc46001b4692eb0b33aa04e0a5f643d56d
RamukYaja2018 <81286681+RamukYaja2018@users.noreply.github.com>
2025-07-25 20:54:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 // app.js
+
 const express = require('express');
+
 const app = express();
-const port = process.env.PORT || 8080; // Use process.env.PORT for flexibility, or directly 8080
+
+const port = process.env.PORT || 8080;
+
 
 app.get('/', (req, res) => {
-  res.send('Hello from GKE CI/CD! Your app is running!');
+
+  res.send('Hello from GKE CI/CD! This is a NEW version deployed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'); // Change this line
+
 });
 
+
 app.listen(port, () => {
+
   console.log(`Node.js app listening on port ${port}`);
+
 });
